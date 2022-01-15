f9e1bfc24317c10c66070f147e6fc1ff46a48d45
Somanath1458 <37767811+Somanath1458@users.noreply.github.com>
2022-01-15 11:55:16 +0530

    "partially done"

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const moment = require('moment')
 
 const app = express()
 
@@ -11,12 +12,12 @@ app.use(express.static('public'))
 mongoose.connect('mongodb://localhost:27017/jigsawDB', { useNewUrlParser: true })
 
 //budgetschema
-const budgetSchema = {
+const budgetSchema = new mongoose.Schema({
 	id: Number,
 	amount: Number,
 	timeperiod: String,
 	sector: String,
-}
+})
 
 const Budget = mongoose.model('Budget', budgetSchema)
 
@@ -32,14 +33,14 @@ app.get('/budget', (req, res) => {
 })
 
 //investschema
-const investmentSchema = {
+const investmentSchema = new mongoose.Schema({
 	id: Number,
 	date: Date,
 	amount: Number,
 	sector: String,
-}
+})
 
-const Investment = mongoose.model('Investment', budgetSchema)
+const Investment = mongoose.model('Investment', investmentSchema)
 
 //view investment data
 app.get('/invest', (req, res) => {
@@ -54,46 +55,135 @@ app.get('/invest', (req, res) => {
 
 //query handling
 
-//for month
-
 Investment.find(function (err, foundInvestments) {
 	if (!err) {
 		let totalBudget = 0,
 			monthBudget = 0,
 			quarterBudget = 0,
-			bigdataBudget = 0,
-			ecommerceBudget = 0
+			ecommerceBudget = 0;
 
 		const a = foundInvestments.map((investments) => {
-			//rule5
-			if (totalBudget < 350) {
-				//rule4
-				if (investments.sector === 'ecommerce') {
-					ecommerceBudget += investments.amount
-					if (ecommerceBudget <= 70) {
-						totalBudget += investments.amount
+			totalBudget += investments.amount
+			if (totalBudget <= 350) {
+				monthBudget += investments.amount
+				//Jan
+				if (
+					moment(investments.date).format('YYYY-MM-DD') >= '2020-01-01' &&
+					moment(investments.date).format('YYYY-MM-DD') <= '2020-01-31' &&
+					monthBudget <= 70
+				) {
+					if (investments.sector === 'ecommerce') {
+						ecommerceBudget += investments.amount
+						if (ecommerceBudget <= 70) {
+							return investments
+						}
+					} else if (investments.sector === 'bigdata') {
+						quarterBudget += investments.amount
+						if (
+							moment(investments.date).format('YYYY-MM-DD') >= '2020-01-01' &&
+							moment(investments.date).format('YYYY-MM-DD') <= '2020-03-31' &&
+							quarterBudget <= 25
+						) {
+							return investments
+						}
+					} else if (investments.sector === 'fintech') {
+						if (investments.amount <= 30) return investments
+					} else {
+						monthBudget=0;
 						return investments
 					}
 				}
-				//rule3
-				if (investments.sector === 'bigdata') {
-					// if(investments.date <= '31-03-2020'){
-					bigdataBudget += investments.amount
-					if (bigdataBudget <= 25) {
-						totalBudget += investments.amount
+				//Feb
+				else if (
+					moment(investments.date).format('YYYY-MM-DD') >= '2020-02-01' &&
+					moment(investments.date).format('YYYY-MM-DD') <= '2020-02-31' &&
+					monthBudget <= 70
+				) {
+					console.log('feb:'+monthBudget)
+					if (investments.sector === 'ecommerce') {
+						ecommerceBudget += investments.amount
+						if (ecommerceBudget <= 70) {
+							return investments
+						}
+					} else if (investments.sector === 'bigdata') {
+						quarterBudget += investments.amount
+						if (
+							moment(investments.date).format('YYYY-MM-DD') >= '2020-01-01' &&
+							moment(investments.date).format('YYYY-MM-DD') <= '2020-03-31' &&
+							quarterBudget <= 25
+						) {
+							return investments
+						}
+					} else if (investments.sector === 'fintech') {
+						if (investments.amount <= 30) return investments
+					} else {
+						monthBudget=0;
 						return investments
 					}
-					// }
 				}
-				//rule2
-				if (investments.sector === 'fintech' && investments.amount <= 30) {
-					totalBudget += investments.amount
-					return investments
+				//March
+				else if (
+					moment(investments.date).format('YYYY-MM-DD') >= '2020-03-01' &&
+					moment(investments.date).format('YYYY-MM-DD') <= '2020-03-31' &&
+					monthBudget <= 70
+				) {
+					if (investments.sector === 'ecommerce') {
+						ecommerceBudget += investments.amount
+						if (ecommerceBudget <= 70) {
+							return investments
+						}
+					} else if (investments.sector === 'bigdata') {
+						quarterBudget += investments.amount
+						if (
+							moment(investments.date).format('YYYY-MM-DD') >= '2020-01-01' &&
+							moment(investments.date).format('YYYY-MM-DD') <= '2020-03-31' &&
+							quarterBudget <= 25
+						) {
+							return investments
+						}
+					} else if (investments.sector === 'fintech') {
+						if (investments.amount <= 30) return investments
+					} else {
+						return investments
+					}
 				}
+				//April
+				else if (
+					moment(investments.date).format('YYYY-MM-DD') >= '2020-04-01' &&
+					moment(investments.date).format('YYYY-MM-DD') <= '2020-04-30' &&
+					monthBudget <= 70
+				) {	
+					quarterBudget = 0		
+					if (investments.sector === 'ecommerce') {
+						ecommerceBudget += investments.amount
+						if (ecommerceBudget <= 70) {
+							return investments
+						}
+					} else if (investments.sector === 'fintech') {
+						if (investments.amount <= 30) return investments
+					} else {
+						return investments
+					}
+				}
+				//May
+				else {
+					if (investments.sector === 'ecommerce') {
+						ecommerceBudget += investments.amount
+						if (ecommerceBudget <= 70) {
+							return investments
+						}
+					} else if (investments.sector === 'fintech') {
+						if (investments.amount <= 30) return investments
+					} else {
+						return investments
+					}
+				}
+				monthBudget -= investments.amount
 			}
+			totalBudget -= investments.amount
 		})
 		console.log(a)
-		console.log(totalBudget, bigdataBudget)
+		console.log(totalBudget, quarterBudget, monthBudget)
 	} else {
 		res.send(err)
 	}
